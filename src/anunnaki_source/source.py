7c2ffcd5fe81cdab2536bf9a3a01e0d01312510a
hasna pasha <38372270+hasanpasha@users.noreply.github.com>
2023-09-08 00:48:24 +0300

    change get_season_list function signature

--- src/anunnaki_source/source.py
+++ src/anunnaki_source/source.py
@@ -1,4 +1,6 @@
-from anunnaki_source.models import Media, Season, Episode, Video, Subtitle
+from anunnaki_source.models import (
+    Media, Season, Episode, Video, Subtitle, Filter
+)
 
 from abc import ABC, abstractmethod
 from typing import Union
@@ -14,7 +16,7 @@ class Source(ABC):
         pass
 
     @abstractmethod
-    def get_season_list(self, media: Media) -> Union[list[Season], Episode]:
+    def get_season_list(self, media: Media) -> list[Season]:
         pass
 
     @abstractmethod
