570e4849d549bf3753beae93267ca167e9c404b1
hasna pasha <38372270+hasanpasha@users.noreply.github.com>
2023-10-07 14:53:45 +0300

    implement Source get functions in CatalogueSource

--- src/anunnaki_source/catalogue_source.py
+++ src/anunnaki_source/catalogue_source.py
@@ -18,30 +18,51 @@ class CatalogueSource(Source):
     async def init_session(self) -> None:
         self.session = ClientSession(headers=self.headers)
 
+    async def get_search_media(self, query: str, page: int, filters: FilterList = None) -> MediasPage:
+        return await self.fetch_search_media(query, page, filters)
+
     @abstractmethod
     async def fetch_search_media(self, query: str, page: int, filters: FilterList = None) -> MediasPage:
         pass
     
+    async def get_popular_media(self, page: int, filters: FilterList = None) -> MediasPage:
+        return await self.fetch_popular_media(page, filters)
+
     @abstractmethod
     async def fetch_popular_media(self, page: int, filters: FilterList = None) -> MediasPage:
         pass
 
+    async def get_latest_media(self, page: int, filters: FilterList = None) -> MediasPage:
+        return await self.fetch_latest_media(page, filters)
+    
     @abstractmethod
     async def fetch_latest_media(self, page: int, filters: FilterList = None) -> MediasPage:
         pass
+ 
+    async def get_detail(self, media: Media) -> Media:
+        return await self.fetch_media_detail(media)
     
     @abstractmethod
     async def fetch_media_detail(self, media: Media) -> Media:
         pass
     
+    async def get_season_list(self, media: Media) -> SeasonList:
+        return await self.fetch_season_list(media)
+    
     @abstractmethod
     async def fetch_season_list(self, media: Media) -> SeasonList:
         pass
+    
+    async def get_video_list(self, episode: Episode) -> VideoList:
+        return await self.fetch_video_list(episode)
     
     @abstractmethod
     async def fetch_video_list(self, episode: Episode) -> VideoList:
         pass
     
+    async def get_subtitle_list(self, episode: Episode) -> SubtitleList:
+        return await self.fetch_subtitle_list(episode)
+
     @abstractmethod
     async def fetch_subtitle_list(self, episode: Episode) -> SubtitleList:
         pass
